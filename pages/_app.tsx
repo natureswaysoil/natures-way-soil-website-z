8c2671cbaffa0bcc082556f9a701522da9dc6d41
natureswaysoil <natureswaysoil@gmail.com>
2025-08-21 23:05:54 -0400

    Add _app.tsx for Next.js application

---
+++ pages/_app.tsx
@@ -0,0 +1,10 @@
+import type { AppProps } from 'next/app';
+import { CartProvider } from '../context/CartContext';
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <CartProvider>
+      <Component {...pageProps} />
+    </CartProvider>
+  );
+}
