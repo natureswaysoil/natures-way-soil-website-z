1154767cb53056357cc6f41d8edeb2a9e2d64676
natureswaysoil <natureswaysoil@gmail.com>
2025-08-21 23:06:45 -0400

    Add next.config.js for image configuration

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-module.exports = nextConfig;
+module.exports = {
+  images: {
+    domains: [], // add domains if using remote images
+  },
+};
