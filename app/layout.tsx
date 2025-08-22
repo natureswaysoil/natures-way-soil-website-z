02b4a50f2958787ec64229af22f8b617d1efafb4
natureswaysoil <natureswaysoil@gmail.com>
2025-08-22 17:30:26 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,16 @@
+import "./globals.css"
+import { CartProvider } from "@/context/CartContext"
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body>
+        <CartProvider>{children}</CartProvider>
+      </body>
+    </html>
+  )
+}
+
 import { CartProvider } from '../lib/cart';
 import Link from 'next/link';
 
