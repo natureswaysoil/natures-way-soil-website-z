c461992cd37fff53b8014f114c51057706cbce3d
natureswaysoil <natureswaysoil@gmail.com>
2025-08-21 23:06:07 -0400

    Add Home component to pages/index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import HeroSection from '../components/HeroSection';
+import ProductGrid from '../components/ProductGrid';
+import CartBar from '../components/CartBar';
+
+export default function Home() {
+  return (
+    <>
+      <HeroSection />
+      <ProductGrid />
+      <CartBar />
+    </>
+  );
+}
